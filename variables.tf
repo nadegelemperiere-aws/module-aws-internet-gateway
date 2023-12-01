0072d13a5505234ff73ac1a732235c6e1bd7470d
Nadege LEMPERIERE <98167010+nadegelemperiere@users.noreply.github.com>
2023-11-30 20:31:06 -0500

    Upgrade  (#3)
    Upgrade terraform
    Upgrade robotframework keywords version
    Upgrade CI/CS version

--- variables.tf
+++ variables.tf
@@ -1,61 +1,67 @@
 # -------------------------------------------------------
-# TECHNOGIX
-# -------------------------------------------------------
-# Copyright (c) [2022] Technogix SARL
+# Copyright (c) [2022] Nadege Lemperiere
 # All rights reserved
 # -------------------------------------------------------
 # Module to deploy the internet gateway structure
 # -------------------------------------------------------
 # Nadège LEMPERIERE, @20 november 2021
-# Latest revision: 20 november 2021
+# Latest revision: 30 november 2023
 # -------------------------------------------------------
 
 # -------------------------------------------------------
 # Contact e-mail for this deployment
 # -------------------------------------------------------
 variable "email" {
-	type 	= string
+	type 	 = string
+	nullable = false
 }
 
 # -------------------------------------------------------
 # Environment for this deployment (prod, preprod, ...)
 # -------------------------------------------------------
 variable "environment" {
-	type 	= string
+	type 	 = string
+	nullable = false
 }
 
 # -------------------------------------------------------
 # Topic context for this deployment
 # -------------------------------------------------------
 variable "project" {
-	type    = string
+	type     = string
+	nullable = false
 }
 variable "module" {
-	type 	= string
+	type 	 = string
+	nullable = false
 }
 
 # -------------------------------------------------------
 # Solution version
 # -------------------------------------------------------
 variable "git_version" {
-	type    = string
-	default = "unmanaged"
+	type     = string
+	default  = "unmanaged"
+	nullable = false
 }
 
 # --------------------------------------------------------
 # VPC identifiers
 # --------------------------------------------------------
 variable "vpc" {
-	type = string
+	type     = string
+	nullable = false
 }
 variable "route_tables" {
-	type = list(string)
+	type     = list(string)
+	nullable = false
 }
 
 # -------------------------------------------------------
 # Egress only
 # -------------------------------------------------------
 variable "egress_only" {
-	type    = bool
-	default = true
+	type     = bool
+	default  = true
+	nullable = false
 }
